c91f59e33d95974ab09fcde2d84a3650dcfcf498
CordtMenzler <92551839+CordtMenzler@users.noreply.github.com>
2021-12-14 17:56:30 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world,ground;
+var ground
+var background, backroundImg
+var tower, towerImg
+var cannonImg
+var cannonBaseImg
+var angle=20
+function preload() {
+ 
+  backgroundImg=loadImage("assets/background.gif")
+ towerImg=loadImage("assets/tower.png")
+}
+function setup() {
+
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  var  options={
+    isStatic:true
+
+  }
+
+  ground=Bodies.rectangle(0,height-1,width*2,1,options)
+  World.add(world,ground)
+  tower=Bodies.rectangle(160,350,160,310,options)
+  World.add(world,tower)
+  cannon=new Cannon(180,110,130,100,angle)
+ 
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg,0,0,1200,600)
+  
+ 
+  Engine.update(engine);
+  rectMode(CENTER)
+  rect(ground.position.x,ground.position.y,width*2,1)
+  
+   Engine.update(engine)
+   push()
+   imageMode(CENTER)
+   image(towerImg,tower.position.x, tower.position.y,160,310)
+   pop()
+
+   cannon.display()
+}
